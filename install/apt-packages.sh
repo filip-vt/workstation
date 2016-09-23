265e943f9c3e9d75adfb2f5a1806c12b2c65043e
filip-vt <filip.vt@gmail.com>
2016-09-23 11:05:04 +0200

    pip package pychef and dependency

--- install/apt-packages.sh
+++ install/apt-packages.sh
@@ -8,6 +8,6 @@ sudo apt purge $DELETE_PACKAGES
 
 # make sure the following is installed
 
-INSTALL_PACKAGES="htop ubuntu-restricted-extras terminator meld leafpad git gimp gparted build-essential ruby ruby-dev python-dev openjdk-9-jre-headless"
+INSTALL_PACKAGES="htop ubuntu-restricted-extras terminator meld leafpad git gimp gparted build-essential ruby ruby-dev python-dev openjdk-9-jre-headless libssl-dev"
 
 sudo apt install $INSTALL_PACKAGES
