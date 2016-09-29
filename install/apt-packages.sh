97a8d5797d5a6c8bb59b70ff33390ded48230ed2
Filip Van Tittelboom <filip.vt@gmail.com>
2016-09-29 13:12:22 +0200

    added jq

--- install/apt-packages.sh
+++ install/apt-packages.sh
@@ -8,6 +8,6 @@ sudo apt purge $DELETE_PACKAGES
 
 # make sure the following is installed
 
-INSTALL_PACKAGES="htop ubuntu-restricted-extras terminator meld leafpad git gimp gparted build-essential ruby ruby-dev python-dev openjdk-9-jre-headless libssl-dev libqt5x11extras5"
+INSTALL_PACKAGES="htop ubuntu-restricted-extras terminator meld leafpad git gimp gparted build-essential ruby ruby-dev python-dev openjdk-9-jre-headless libssl-dev libqt5x11extras5 jq"
 
 sudo apt install $INSTALL_PACKAGES
