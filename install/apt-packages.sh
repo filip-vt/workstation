e3dbce3f2e4b85be5229e39413437c37e7fc461e
Filip Van Tittelboom <filip.vt@gmail.com>
2016-11-24 14:39:13 +0100

    added powerline apt package

--- install/apt-packages.sh
+++ install/apt-packages.sh
@@ -8,6 +8,6 @@ sudo apt purge $DELETE_PACKAGES
 
 # make sure the following is installed
 
-INSTALL_PACKAGES="htop ubuntu-restricted-extras terminator meld leafpad git gimp gparted build-essential ruby ruby-dev python-dev openjdk-9-jre-headless libssl-dev libqt5x11extras5 jq nodejs-legacy npm virtualbox zsh"
+INSTALL_PACKAGES="htop ubuntu-restricted-extras terminator meld leafpad git gimp gparted build-essential ruby ruby-dev python-dev openjdk-9-jre-headless libssl-dev libqt5x11extras5 jq nodejs-legacy npm virtualbox zsh powerline"
 
 sudo apt install $INSTALL_PACKAGES
